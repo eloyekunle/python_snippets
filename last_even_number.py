7c25aa9dd85805c5b960c23d686c9fd508b7f7d8
Oyekunle Elijah PlayMice <elijah@playmiceweb.com>
2017-03-14 21:36:29 +0100

    Algorithm to get last even number in a sequence

--- last_even_number.py
+++ last_even_number.py
@@ -1,3 +1,7 @@
+# An algorithm that takes as input a list of n integers and finds the location of the last even integer in the
+# list or returns 0 if there are no even integers in the list.
+
+
 def last_even_number(numbers):
     index = None
     for i in range(len(numbers)):
@@ -9,4 +13,7 @@ def last_even_number(numbers):
 
 nums = list(map(int, input("Enter a sequence of numbers, separated by space: ").split()))
 last_even = last_even_number(nums)
-print("The last even number is", last_even[0], "at index", last_even[1])
+if last_even:
+    print("The last even number is", last_even[0], "at index", last_even[1])
+else:
+    print(0)
