da2b97edfdaba8c968a82f64c8e48827461039b6
Oyekunle Elijah PlayMice <elijah@playmiceweb.com>
2017-03-15 10:19:13 +0100

    Completed exponential algorithm

---
+++ exponential.py
@@ -0,0 +1,20 @@
+# an algorithm to compute x^n , where x is a real number and n is an integer.
+
+
+def exponential(num, exponent):
+    number = num
+    abs_exponent = abs(exponent)
+    while abs_exponent > 1:
+        num *= number
+        abs_exponent -= 1
+    if exponent == 0:
+        return 1
+    if exponent < 0:
+        return 1 / num      # Returns 1/x^n
+
+    return num
+
+
+number = list(map(int, input("Enter a number and the exponent, space separated: ").split()))
+exponent = exponential(number[0], number[1])
+print(exponent)
