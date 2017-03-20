103bfeaf086116e3687c0eb1c99b0689af3af25f
Oyekunle Elijah PlayMice <elijah@playmiceweb.com>
2017-03-20 16:49:06 +0100

    Added binary insertion

--- insert_into_list_binary.py
+++ insert_into_list_binary.py
@@ -4,11 +4,29 @@
 
 def insert_binary(key, numbers):
     i = 0
-    j = len(numbers) - 1
+    numbers.append(key)
+    length = len(numbers)
+    j = length - 1
 
     while i < j:
         middle = (i + j) // 2
-        if key > middle:
+        if key > numbers[middle]:
             i = middle + 1
+            print(numbers[i:j+1])
         else:
             j = middle
+            print(numbers[i:j+1])
+    if key <= numbers[i]:
+        location = i
+        for k in range(1, length - 1):
+            numbers[length - k] = numbers[length - k - 1]
+        numbers[i] = key
+    else:
+        location = i + 1
+    print("Element will be inserted at", location)
+    print(numbers)
+
+
+nums = list(map(int, input("Enter numbers, separated by space: ").split()))
+insert_key = int(input("Enter number to insert: "))
+insert_binary(insert_key, nums)
