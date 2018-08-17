58367cb3ad415f6a23312d3fb7cb925de8ab5c8b
Elijah Oyekunle <elijah.oyekunle@ehealthnigeria.org>
2018-08-17 15:28:24 +0100

    updated with 2 codility solutions

---
+++ time_complexity/frog_jmp.py
@@ -0,0 +1,12 @@
+from math import ceil
+
+
+def solution(x, y, d):
+    return ceil((y-x) / d)
+
+
+if __name__ == '__main__':
+    assert solution(10, 85, 30) == 3
+    assert solution(0, 30, 40) == 1
+    assert solution(0,0,40) == 0
+    assert solution(30,40,0) == 0
