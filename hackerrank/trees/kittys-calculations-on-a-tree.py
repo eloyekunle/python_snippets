bd6a7088e88d81918ead0ea7031122097303ddd0
Elijah Oyekunle <eloyekunle@gmail.com>
2018-09-02 18:07:44 +0100

    faster but incorrect

--- hackerrank/trees/kittys-calculations-on-a-tree.py
+++ hackerrank/trees/kittys-calculations-on-a-tree.py
@@ -8,6 +8,7 @@ class Tree:
     def __init__(self):
         self.root = None
         self.inserted = {}
+        self.distances = defaultdict(lambda: defaultdict(int))
 
     def __len__(self):
         return len(self.inserted)
@@ -21,12 +22,19 @@ class Tree:
             self.inserted[self.root.data] = self.root
 
         not_inserted = {node1, node2} - set(self.inserted.values())
-        inserted = self.inserted[list({node1,node2} - not_inserted)[0].data]
+        parent = self.inserted[list({node1,node2} - not_inserted)[0].data]
         not_inserted_node = list(not_inserted)[0]
-        not_inserted_node.level = inserted.level + 1
-        not_inserted_node.parent = inserted.data
-        inserted.children[not_inserted_node.data] = not_inserted_node
-        self.inserted[not_inserted_node.data] = inserted.children[not_inserted_node.data]
+        not_inserted_node.level = parent.level + 1
+        not_inserted_node.parent = parent.data
+        parent.children[not_inserted_node.data] = not_inserted_node
+        self.inserted[not_inserted_node.data] = parent.children[not_inserted_node.data]
+        while parent:
+            for sibling in parent.children.keys():
+                self.distances[sibling][not_inserted_node.data] = self.distances[not_inserted_node.data][sibling] = not_inserted_node.level - parent.level + 1
+            if parent.parent is None:
+                self.distances[parent.data][not_inserted_node.data] = self.distances[not_inserted_node.data][parent.data] = not_inserted_node.level - parent.level
+                break
+            parent = self.inserted[parent.parent]
 
 class Node:
     def __init__(self, data):
@@ -44,32 +52,14 @@ class Node:
 def calc_distance(v1, v2):
     v1_node = inserted[v1]
     v2_node = inserted[v2]
-
     if distances[v1][v2]:
         return distances[v1][v2]
     elif distances[v2][v1]:
         return distances[v2][v1]
-
-    parents_1 = set()
-    parent_1 = v1_node.data
-    while parent_1 is not None:
-        inserted_parent_1 = inserted[parent_1]
-        distances[v1][parent_1] = distances[parent_1][v1] = v1_node.level - inserted_parent_1.level
-        parents_1.add(inserted_parent_1)
-        parent_1 = inserted_parent_1.parent
-
-    parents_2 = set()
-    parent_2 = v2_node.data
-    while parent_2 is not None:
-        inserted_parent_2 = inserted[parent_2]
-        distances[v2][parent_2] = distances[parent_2][v2] = v2_node.level - inserted_parent_2.level
-        parents_2.add(inserted_parent_2)
-        parent_2 = inserted_parent_2.parent
-
-    len_common_parents = len(parents_1 & parents_2)
-    distance = (v1_node.level - len_common_parents) + (v2_node.level - len_common_parents)
-    distances[v1][v2] = distances[v2][v1] = distance
-    return distance
+    else:
+        distance = calc_distance(v1_node.parent, v2_node.parent) + 2
+        distances[v1][v2] = distances[v2][v1] = distance
+        return distance
 
 def kittyCalc(set_pairs):
     pairs = get_pairs_from_set(set_pairs)
@@ -86,11 +76,11 @@ if __name__ == '__main__':
         pair = list(map(int, input().split()))
         t.create(*pair)
     inserted = t.inserted
-    distances = defaultdict(lambda: defaultdict(int))
+    distances = t.distances
     for i in range(q):
         _ = input()
         k_set = set(map(int, input().split()))
-        print(i, kittyCalc(k_set))
+        print(kittyCalc(k_set))
 
 # if __name__ == '__main__':
 #     nodes = [[1,2], [3,1], [4,1], [3,5], [3,6], [3,7]]
@@ -98,7 +88,7 @@ if __name__ == '__main__':
 #     for x,y in nodes:
 #         t.create(x,y)
 #     inserted = t.inserted
-#     distances = defaultdict(dict)
+#     distances = t.distances
 #     assert kittyCalc({2,4}) == 16
 #     assert kittyCalc({2,1}) == 2
 #     assert kittyCalc({5}) == 0
