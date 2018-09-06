badec9a7ebd03eee8bd8f33d5ba0566d64495604
Elijah Oyekunle <elijah.oyekunle@ehealthnigeria.org>
2018-09-06 13:00:21 +0100

    finished abbr, added test cases with sols

--- hackerrank/dynamic_programming/abbr.py
+++ hackerrank/dynamic_programming/abbr.py
@@ -1,36 +1,34 @@
 #!/bin/python3
 
 def abbreviation(a, b):
-    c_a = c_b = None
     len_a = len(a)
     len_b = len(b)
-    s = [[0] * (len_a + 1) for _ in range(len_b + 1)]
-    # Loop over target string
-    for i in range(len_b):
-        c_b = b[i]
-        c_b_upper = c_b.isupper()
-        s[i][0] = i
-        # Compare original string, start from current index in target string.
+    s = [[False] * (len_a + 1) for _ in range(len_b + 1)]
+    s[0][0] = True
+
+    for k in range(1, len_a + 1):
+        if a[k-1].islower():
+            s[0][k] = s[0][k-1]
+
+    for i in range(1, len_b + 1):
+        c_b = b[i-1]
         for j in range(1, len_a + 1):
             c_a = a[j-1]
-            if i == 0:
-                s[i][j] = j
-            elif j < i:
-                s[i][j] = 1001
-            elif c_b == c_a:
-                s[i][j] = min(s[i-1][j], s[i][j-1], s[i-1][j-1])
-            elif c_b_upper and c_a.upper() == c_b:
-                s[i][j] = min(s[i-1][j], s[i][j-1], s[i-1][j-1]) + 1
-            elif 9999 in (s[i-1][j], s[i][j-1], s[i-1][j-1]):
-                s[i][j] = 9999
+            if c_a.isupper():
+                if c_a == c_b:
+                    s[i][j] = s[i-1][j-1]
+            elif c_a.upper() == c_b:
+                s[i][j] = s[i-1][j-1] or s[i][j-1]
             else:
-                s[i][j] = 9999
-    print(s)
-    return 'YES'
+                s[i][j] = s[i][j-1]
+    if s[-1][-1]:
+        return 'YES'
+    return 'NO'
 
 if __name__ == '__main__':
-    f = open('/home/elijah/MyCode/python_snippets/data/abbr-001.txt')
+    f = open('/home/elijah/MyCode/python_snippets/data/abbr-003.txt')
     q = int(f.readline())
+    results = open('../../data/abbr-003.sol.txt').readlines()
 
     for q_itr in range(q):
         a = f.readline().rstrip()
@@ -39,4 +37,5 @@ if __name__ == '__main__':
 
         result = abbreviation(a, b)
 
-        print(result)
+        result_correct = results[q_itr].rstrip()
+        assert result == result_correct
