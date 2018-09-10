04a2ef3d03d51909665fa28a3a1bea98cc63d0dd
Elijah Oyekunle <elijah.oyekunle@ehealthnigeria.org>
2018-09-10 08:38:05 +0100

    added datasets and code stub

---
+++ hackerrank/graphs/torque-and-development.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the roadsAndLibraries function below.
+def roadsAndLibraries(n, c_lib, c_road, cities):
+    pass
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        nmC_libC_road = input().split()
+
+        n = int(nmC_libC_road[0])
+
+        m = int(nmC_libC_road[1])
+
+        c_lib = int(nmC_libC_road[2])
+
+        c_road = int(nmC_libC_road[3])
+
+        cities = []
+
+        for _ in range(m):
+            cities.append(list(map(int, input().rstrip().split())))
+
+        result = roadsAndLibraries(n, c_lib, c_road, cities)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
