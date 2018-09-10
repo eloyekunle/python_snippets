76b720283bccbdc60176c97ca392243311dc61f2
Elijah Oyekunle <elijah.oyekunle@ehealthnigeria.org>
2018-09-10 15:42:20 +0100

    added stack ds

---
+++ data-structures/stack.py
@@ -0,0 +1,17 @@
+class Stack:
+    def __init__(self):
+        self._items = []
+    def __len__(self):
+        return len(self._items)
+    def push(self, x):
+        self._items.append(x)
+    def pop(self, x):
+        if self.is_empty():
+            raise IndexError('Stack is empty.')
+        return self._items.pop()
+    def top(self):
+        if self.is_empty():
+            raise IndexError('Stack is empty.')
+        return self._items[-1]
+    def is_empty(self):
+        return len(self._items) == 0
