3c1af9633df15bafd99d927b547d0123d31b2f87
Elijah Oyekunle <eloyekunle@gmail.com>
2018-09-18 20:37:29 +0100

    updated floyd

--- algorithms/floyd_warshall.py
+++ algorithms/floyd_warshall.py
@@ -21,19 +21,20 @@ def floyd_warshall_g(g: Graph) -> List:
     return floyd_warshall(m)
 
 
-# Can be modified later to return another matrix from which we can compute the path.
 def floyd_warshall(m: List) -> List:
     n = len(m)
-    d_matrices : Dict[int, List] = {0: m}
 
-    for k in range(1, n + 1):
-        m_cur = d_matrices[k] = [[] for _ in range(n)]
-        m_prev = d_matrices[k-1]
+    for k in range(n):
+        kk = m[k]
         for i in range(n):
+            ii = m[i]
+            ik = ii[k]
+            if ik == math.inf:
+                continue
             for j in range(n):
-                m_cur[i].append(min(m_prev[i][j], m_prev[i][k] + m_prev[k][j]))
+                kj = kk[j]
+                if kj == math.inf:
+                    continue
+                m[i][j] = min(ii[j], ik + kj)
 
-        # Space efficiency.
-        del d_matrices[k-1]
-
-    return d_matrices[n]
+    return m
