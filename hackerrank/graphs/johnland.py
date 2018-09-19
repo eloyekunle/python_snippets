cf16622ea6fe79c3804272a599d759195f9c7b6f
Elijah Oyekunle <eloyekunle@gmail.com>
2018-09-19 21:51:58 +0100

    switched johnland to kruskal from prim's

--- hackerrank/graphs/johnland.py
+++ hackerrank/graphs/johnland.py
@@ -2,69 +2,71 @@ import os
 import sys
 import math
 from collections import defaultdict
-from data_structures.heap import MinHeapPriorityQueue
-from data_structures.graph_no_vertice_object import Graph
 
-def roadsInHackerland(n, edges, start=1):
-    def dfs(u):
+from data_structures.partition_min import Partition
+
+class Edge:
+    __slots__ = '_origin', '_destination', '_element'
+
+    def __init__(self, u, v, x):
+        self._origin = u
+        self._destination = v
+        self._element = x   # sometimes acts as weight for weighted graphs
+
+    def endpoints(self):
+        return self._origin, self._destination  # Order matters yo!
+
+    def opposite(self, v):
+        return self._destination if v == self._origin else self._origin
+
+    def element(self):
+        return self._element
+
+    def __hash__(self):  # will allow edge to be a map/set key
+        return hash((self._origin, self._destination))
+
+def roadsInHackerland(n, edges):
+    def dfs(u, parent=None):
         total = 1
 
-        if u in tree:
-            for edge in tree[u]:
-                v = edge.opposite(u)
-                d_edges[edge] = dfs(v)
-                total += d_edges[edge]
+        for edge in tree[u]:
+            if parent is not None and parent in edge.endpoints():
+                continue
+            v = edge.opposite(u)
+            d_edges[edge] = dfs(v, u)
+            total += d_edges[edge]
 
         return total
 
-    d = {}
-    pq = MinHeapPriorityQueue()
-    pqlocator = {}
-    tree = defaultdict(list)
-    g = Graph()
     mst = []
+    tree = defaultdict(list)
 
-    for i in range(1, n + 1):
-        if i == start:
-            d[start] = 0
-        else:
-            d[i] = math.inf
-        pqlocator[i] = pq.add(d[i], (i, None))
-        g.insert_vertex(i)
+    while len(mst) != m - 1 and len(edges) != 0:
+        u,v,w = edges.pop()
+        edge = Edge(u,v,w)
+        a = forest.find(position[u])
+        b = forest.find(position[v])
 
-    for j in range(m):
-        g.insert_edge(*edges[j])
-
-    # PRIM's MST
-    while not pq.is_empty():
-        k,v = pq.remove_top()
-        u,e = v
-        del pqlocator[u]
-        if e is not None:
-            tree[e.opposite(u)].append(e)
-            mst.append(e)
-        for edge in g.incident_edges(u):
-            v = edge.opposite(u)
-            if v in pqlocator:
-                wt = edge.element()
-                if wt < d[v]:
-                    d[v] = wt
-                    pq.update(pqlocator[v], d[v], (v, edge))
+        if a != b:
+            mst.append(edge)
+            tree[u].append(edge)
+            tree[v].append(edge)
+            forest.union(a,b)
 
     d_edges = {x:0 for x in mst}
 
-    dfs(1)
+    dfs(next(iter(tree)))
 
     results = [(d_edges[e] * (n - d_edges[e])) for e in mst]
 
     sum = 0
     for i in range(len(results)):
-        sum += results[i] * (2 ** mst[i].element())
+        sum += results[i] * (1 << mst[i].element())     # 1 << n  ==  2 ** n
 
     return bin(sum)[2:]
 
 if __name__ == '__main__':
-    f = open('../data/johnland-002.txt')
+    f = open('../data/johnland-001.txt')
 
     nm = f.readline().split()
 
@@ -74,9 +76,12 @@ if __name__ == '__main__':
 
     edges = []
 
-    for _ in range(m):
+    forest = Partition()
+    position = {}
+    for i in range(m):
+        position[i+1] = forest.make_group(i+1)
         edges.append([int(x) for x in f.readline().split()])
 
-    result = roadsInHackerland(n, edges)
+    result = roadsInHackerland(n, sorted(edges, key=lambda edge: edge[2], reverse=True))
 
     print(result)
